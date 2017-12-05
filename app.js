86080fa731798b111b3a477847868efc3c1b7bf9
sumire21 <lily7anne@gmail.com>
2017-12-05 14:55:21 +0000

    chnaged IP address detection and added stripping from IPv6

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get("/whoami", function(req, res){
   // console.log(os.networkInterfaces().eth0[0].address);
 
    res.json({
-       "IP Address": req.ip,
+       "IP Address": req.connection.remoteAddress.replace(/^.*:/, ''),
        "Language": req.get('Accept-Language').slice(0,5),
        "Software": os.type()+" "+os.release()
    });
