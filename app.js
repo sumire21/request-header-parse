dd113a10a2017953eae9f1dff5ce95fb6190b646
sumire21 <lily7anne@gmail.com>
2017-12-05 15:03:22 +0000

    changed getting IP address

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var os = require('os');
 
 app.get("/whoami", function(req, res){
   // console.log(os.networkInterfaces().eth0[0].address);
+    //console.log(req.socket.localAddress);
+
 
    res.json({
-       "IP Address": req.connection.remoteAddress.replace(/^.*:/, ''),
+       "IP Address": req.socket.localAddress,
        "Language": req.get('Accept-Language').slice(0,5),
        "Software": os.type()+" "+os.release()
    });
